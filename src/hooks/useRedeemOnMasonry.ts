976057d732220b28fa11a9314c982539a681a9fd
Jackson <johnjackson292101@gmail.com>
2022-05-14 01:57:38 +0300

    Minor fixes

--- src/hooks/useRedeemOnMasonry.ts
+++ src/hooks/useRedeemOnMasonry.ts
@@ -7,7 +7,7 @@ const useRedeemOnMasonry = (description?: string) => {
   const handleTransactionReceipt = useHandleTransactionReceipt();
 
   const handleRedeem = useCallback(() => {
-    const alertDesc = description || 'Redeem SNOSHARE from boardroom';
+    const alertDesc = description || 'Redeem WSHARE from boardroom';
     handleTransactionReceipt(tombFinance.exitFromMasonry(), alertDesc);
   }, [tombFinance, description, handleTransactionReceipt]);
   return { onRedeem: handleRedeem };
--- src/hooks/useStakeToMasonry.ts
+++ src/hooks/useStakeToMasonry.ts
@@ -8,7 +8,7 @@ const useStakeToMasonry = () => {
 
   const handleStake = useCallback(
     (amount: string) => {
-      handleTransactionReceipt(tombFinance.stakeShareToMasonry(amount), `Stake ${amount} SNOSHARE to the boardroom`);
+      handleTransactionReceipt(tombFinance.stakeShareToMasonry(amount), `Stake ${amount} WSHARE to the boardroom`);
     },
     [tombFinance, handleTransactionReceipt],
   );
