7ddeef87662571e7d9572f07a9e6539b5445a237
jumpman <jumpman23x@protonmail.com>
2022-08-05 11:02:37 -0400

    btn

--- src/views/Nodes/SnoNodes.jsx
+++ src/views/Nodes/SnoNodes.jsx
@@ -10,10 +10,13 @@ import NrwlLPCard from './NrwlLPCard';
 import { createGlobalStyle } from 'styled-components';
 import HomeImage from '../../assets/img/SVG_Icons_and_web_bg/bg.svg';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 
 const leaderboard =
 'https://app.frozenwalrus.finance/leaderboard'
+
 const BackgroundImage = createGlobalStyle`
   body, html {
     background: url(${HomeImage}) no-repeat !important;
@@ -21,22 +24,45 @@ const BackgroundImage = createGlobalStyle`
   }
 `;
 
+const useStyles = makeStyles((theme) => ({
+  
+  leaderboardbutton: {},
+  '@media only screen and (max-width: 1000px)': {
+    leaderboardbutton: {
+      fontSize: '1.5rem',
+      padding: '10px', 
+      borderRadius: '20px',  
+      minWidth: '25%'
+    },
+  },
+  '@media only screen and (min-width: 1001px)': {
+    leaderboardbutton: {
+      fontSize: '1.5rem',
+      padding: '10px', 
+      borderRadius: '20px',  
+      minWidth: '25%'
+    },
+  },
+}));
+ 
 const SnoNodes = () => {
   const {path} = useRouteMatch();
-
+  const classes = useStyles();
   return (
     <Page>
          <BackgroundImage />
       <Switch>
         <Route exact path={path}>
-        <h2 style={{fontSize: '6vw', textAlign: 'center'}}>NODES</h2>
+        <h2 style={{fontSize: '5vw', textAlign: 'center'}}>NODES</h2>
+
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <Button
           color="primary"
           target="_blank"
           href={leaderboard}
           variant="contained"
-          style={{ marginTop: '5%', borderRadius: '12px', padding: '5px', width: '25%', fontSize: '1.2vw', textAlign: 'center' }}
+          style={{ marginTop: '5%' }}
+          className={classes.leaderboardbutton}
         >
           Go To Leaderboard!
         </Button>
